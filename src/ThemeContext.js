e012438720960e34fbc993ac01050c1a4560ed0c
shashimehta03 <itsmeshashi29@gmail.com>
2024-12-05 19:29:02 +0530

    till twelve done

---
+++ src/ThemeContext.js
@@ -0,0 +1,7 @@
+import { Children, createContext } from "react";
+
+const ThemeContext=createContext;
+
+const ThemeProvider=({Children})=>{
+    
+}
--- src/Twelve.js
+++ src/Twelve.js
@@ -1,9 +1,19 @@
-import React from "react"
+import React, { useState } from "react"
+import "./Twelve.css";
 
 const Twelve = (props) => {
+    const [mode ,setmode]=useState(false);
+    const handelmode=()=>{
+        setmode(!mode);
+    };
   return (
-    <div>
-      
+    <div className={mode? "dark-theme" : "light-theme"}>
+      <h1>Light Mode and Dark Mode</h1>
+      <div>
+        <h3>this shows the mode of area</h3>
+        <input type="checkbox" onChange={handelmode} />
+      </div>
+      <p>{mode?"Dark":"Light"}</p>
     </div>
   )
 };
